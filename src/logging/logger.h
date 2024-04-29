b73785244dc8992f1f78ba19e0924ff196e0f327
Michael Hohenstein <michael@hohenste.in>
2024-04-29 14:51:05 +0200

    added simple logging directives

---
+++ src/logging/logger.h
@@ -0,0 +1,66 @@
+/*
+ * logger.h
+ *
+ *  simple logging directives based on a given log level
+ *  compile with -DLOG_LEVEL=LOG_DEBUG to enable debug logging
+ *  or choose any other level
+ *  */
+
+#pragma once
+#include <fstream>
+
+// every logging level contains the levels below it
+#define LOG_DEBUG 1
+#define LOG_INFO 2
+#define LOG_WARNING 3
+#define LOG_ERROR 4
+#define LOG_NOLOG 5
+
+// if there is no LOG level defined, set it to LOG_NOLOG
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_NOLOG
+#endif
+
+template <typename T> class logger {
+private:
+    std::ofstream logfile;
+    void write(const T& message);
+public:
+    logger(const std::string& filename) {
+        logfile.open(filename, std::ios::app); // append to file
+    }
+    ~logger() {
+        if (logfile.is_open()) {
+            logfile.close();
+        }
+    }
+
+    void debug(const T& message) {
+#if LOG_LEVEL <= LOG_DEBUG
+        write("[DEBUG] " + message);
+#endif
+    }
+
+    void info(const T& message) {
+#if LOG_LEVEL <= LOG_INFO
+        write("[INFO] " + message);
+#endif
+    }
+
+    void warning(const T& message) {
+#if LOG_LEVEL <= LOG_WARNING
+        write("[WARNING] " + message);
+#endif
+    }
+
+    void error(const T& message) {
+#if LOG_LEVEL <= LOG_ERROR
+        write("[ERROR] " + message);
+#endif
+    }
+};
+
+template <typename T>
+void logger<T>::write(const T& message) {
+    logfile << message << std::endl;
+}
---
+++ src/logging/logger_test.cpp
@@ -0,0 +1,10 @@
+#include "logger.h"
+
+int main() {
+    logger<std::string> log("log.txt");
+    log.debug("This is a debug message");
+    log.info("This is an info message");
+    log.warning("This is a warning message");
+    log.error("This is an error message");
+    return 0;
+}
